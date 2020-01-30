0d456f0f248120b520d8e9711718d1be2a7cdfe9
esawan <41877920+esawan@users.noreply.github.com>
2020-01-30 00:11:02 -0800

    Initial commit

---
+++ todoapp/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Todos from './Todos'
+
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play games'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="center blue-text">Todo</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
+} 
+
+export default App;
